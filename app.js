84612ce083509a71f64bcf696ba0053e449c5964
Amit Kumar Sanghi <94451087+Amit-Sanghi@users.noreply.github.com>
2021-12-05 18:55:36 +0530

    update input output

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 var translate = document.querySelector("#translate")
 var txtInput = document.querySelector("#txt-input")
+var output = document.querySelector("#output")
 
 
 
 function clickEventListener() {
     console.log("clicked!!")
     console.log("input ", txtInput.value)
+    output.innerText = "hjgafgiug "+ txtInput.value;
 
 }
 
